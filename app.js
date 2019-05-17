45167afab2f19d16810f2c81c5387a0de53727b0
Zhi <zg.lequint@gmail.com>
2019-05-16 17:36:43 -0700

    adding node server for heroku

---
+++ app.js
@@ -0,0 +1,15 @@
+const express    = require('express');
+
+const port = process.env.PORT || 8080;        // set our port
+
+
+// Serve frontend
+app.use("/", express.static(__dirname + '/public'));
+
+
+// START THE SERVER
+// =============================================================================
+const server = app.listen(port, () => {
+    const host = server.address().address;
+    console.log(`Server listening at http://${host}:${port}`);
+})
