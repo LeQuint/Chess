49ea4a21ce4af5809e9bab27a4e90b3b376895d9
Zhi Guo <zg.lequint@gmail.com>
2019-08-24 15:50:21 -0700

    Delete keys.js

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-dbPassword = 'mongodb+srv://zhichaog:' + encodeURIComponent('Berlin_1989') + '@users-qsama.mongodb.net/test?retryWrites=true&w=majority';
-
-module.exports = {
-    mongoURI: dbPassword
-};
